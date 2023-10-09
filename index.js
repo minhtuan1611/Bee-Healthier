6d746ebece89ff36b47c5c0d26dcdb6199ed323d
minhtuan1611 <tuan.mn51@gmail.com>
2023-10-09 23:11:05 +0300

    fix: url 1.2

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const errorHandler = (error, request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 app.get('/api/users', (req, res) => {
   User.find({}).then((users) => {
